b6ba23371a982f80727457fa53831f815e8fca23
Younès Adem <younes.adem@lnds.lu>
2024-02-10 13:15:47 +0100

    feat: setup spring application

---
+++ src/main/java/com/punly/PunlyBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.punly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunlyBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunlyBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/punly/PunlyBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.punly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunlyBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
